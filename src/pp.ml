0562882b290332e8047d2323072280cf4f9c05a3
Sehyuk Ahn <shyukahn@gmail.com>
2025-11-04 01:20:00 +0900

    extended implementation with dependent sum types

--- src/pp.ml
+++ src/pp.ml
@@ -7,8 +7,14 @@ let rec string_of_exp e =
       "(" ^ string_of_exp e1 ^ " " ^ string_of_exp e2 ^ ")"
   | Abs (x, body) ->
       "λ" ^ x ^ ". " ^ string_of_exp body
+  | Pair (e1, e2) ->
+      "pair(" ^ string_of_exp e1 ^ ", " ^ string_of_exp e2 ^ ")"
+  | Fst e -> "fst(" ^ string_of_exp e ^ ")"
+  | Snd e -> "snd(" ^ string_of_exp e ^ ")"
   | Pi (x, a, b) ->
       "Π" ^ x ^ ":" ^ string_of_exp a ^ ". " ^ string_of_exp b
+  | Sigma (x, a, b) ->
+      "Σ(" ^ x ^ ":" ^ string_of_exp a ^ ")" ^ string_of_exp b
   | Let (x, e1, a, e2) ->
       "let " ^ x ^ " : " ^ string_of_exp a ^ " = " ^ string_of_exp e1 ^
       " in " ^ string_of_exp e2
@@ -20,6 +26,8 @@ let rec string_of_val v =
   | VGen k -> "g" ^ string_of_int k
   | VApp (v1, v2) ->
       "(" ^ string_of_val v1 ^ " " ^ string_of_val v2 ^ ")"
+  | VFst v -> "fst(" ^ string_of_val v ^ ")"
+  | VSnd v -> "snd(" ^ string_of_val v ^ ")"
   | VClos (_, Abs (x, body)) ->
       "λ" ^ x ^ ". " ^ string_of_exp body
   | VClos (_, Pi (x, a, b)) ->
--- src/syntax.ml
+++ src/syntax.ml
@@ -4,14 +4,20 @@ type exp =
   | Var of id
   | App of exp * exp (* M N *)
   | Abs of id * exp (* λx. M *)
-  | Let of id * exp * exp * exp  (* let x = M : A in N *)
-  | Pi  of id * exp * exp (* (x: M) N *)
+  | Pair of exp * exp (* pair(M, N) *)
+  | Fst of exp (* fst(M) *)
+  | Snd of exp (* snd(M) *)
+  | Let of id * exp * exp * exp (* let x = M : A in N *)
+  | Pi of id * exp * exp (* (x: M) N *)
+  | Sigma of id * exp * exp (* Σ(x : M) N *)
   | Type
 
 (* p.170 2.1. Expressions and values *)
 type value =
   | VGen of int (* generic values, the set of new variables *)
   | VApp of value * value
+  | VFst of value
+  | VSnd of value
   | VType
   | VClos of env * exp
 
